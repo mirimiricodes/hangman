5d5bdcd48dce96890ba44b4807e687defbea42f8
Mireia <mireia.ma.ar@gmail.com>
2024-02-24 17:21:16 +0100

    feat: game over

--- src/hangman.js
+++ src/hangman.js
@@ -1,6 +1,7 @@
 let wordToGuess = ""
 let usedLetters = []
 let guessedLetters = []
+let wrongGuesses = 0
 
 function setWordToGuess(word) {
     wordToGuess = word
@@ -15,13 +16,33 @@ function guessLetter(letter) {
     if (wordToGuess.includes(letter)) {
         guessedLetters.push(letter)
     }
+    else {
+        wrongGuesses++
+    }
 }
 
 function getUsedLetters() {
     return usedLetters
 }
+
 function getGuessedLetters() {
     return guessedLetters
+}
+
+function isGameOver() {
+    console.log(wrongGuesses)
+    if (wrongGuesses == 7) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+function resetGame() {
+    wordToGuess = ""
+    usedLetters = []
+    guessedLetters = []
+    wrongGuesses = 0
 }
 
 export default {
@@ -30,5 +51,7 @@ export default {
     guessLetter,
     getUsedLetters,
     getGuessedLetters,
+    isGameOver,
+    resetGame,
 
 }
--- test/hangman.test.js
+++ test/hangman.test.js
@@ -2,6 +2,9 @@ import * as chai from "chai"
 import hangman from "./../src/hangman.js"
 let expect = chai.expect
 describe("Hangman", () => {
+    afterEach(() => {
+        hangman.resetGame()
+    })
     it("should return the number of characters the word has", () => {
         hangman.setWordToGuess("tomato")
         let wordLength = hangman.getWordLength()
@@ -21,5 +24,21 @@ describe("Hangman", () => {
         expect(guessedLetters).includes("t")
 
     })
+    it("should end game after 7 wrong guesses", () => {
+        hangman.setWordToGuess("tomato")
+        hangman.guessLetter("s")
+        hangman.guessLetter("n")
+        hangman.guessLetter("x")
+        hangman.guessLetter("y")
+        hangman.guessLetter("p")
+        hangman.guessLetter("b")
+        hangman.guessLetter("j")
+        expect(hangman.isGameOver()).to.be.equal(true)
 
+    })
+    it("should not end game after 0 wrong guesses", () => {
+        hangman.setWordToGuess("tomato")
+        expect(hangman.isGameOver()).to.be.equal(false)
+
+    })
 })
