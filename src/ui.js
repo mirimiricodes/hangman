8657ede13e41afadd47d8f16868e7d2f9afcd8fb
Mireia <mireia.ma.ar@gmail.com>
2024-03-03 21:51:36 +0100

    feat: show hidden word

--- src/ui.js
+++ src/ui.js
@@ -1,2 +1,45 @@
+import readline from "readline-sync"
+import hangman from "./game/hangman.js"
+
 console.log("Welcome to Mirimiri's hangman!")
-console.log("Press any key to start a new game.")
+readline.question("Press enter to start a new game.")
+
+hangman.initialize()
+
+function showHiddenWord() {
+    let wordLength = hangman.getWordLength()
+    let hiddenWord = ""
+    for (let i = 0; i < wordLength; i++) {
+        hiddenWord = hiddenWord.concat("_ ")
+    }
+    console.log(hiddenWord)
+
+}
+
+function showUsedLetters() {
+
+}
+
+function showTurnsLeft() {
+
+}
+
+function showGameOver() {
+
+}
+
+function showScreen() {
+    console.log("Guess the word!")
+    showHiddenWord()
+    showUsedLetters()
+    showTurnsLeft()
+    let letter = readline.question("Press a letter!")
+    hangman.guessLetter(letter)
+}
+while (!hangman.isGameOver()) {
+    showScreen()
+}
+showGameOver()
+
+
+
